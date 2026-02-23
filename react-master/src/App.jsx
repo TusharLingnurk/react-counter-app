e719def870b8e3430f0fd5dc70f813bc90e8cf47
tushar <tusharlingnurkar@gmail.com>
2026-02-23 21:42:14 +0530

    Added dynamic background color logic

--- react-master/src/App.jsx
+++ react-master/src/App.jsx
@@ -2,18 +2,26 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  
-  const backgroundColor = count > 5 ? "#b8edc4" : "#b26b6b";
+
+  let backgroundColor = "#e2abe4";
+
+if (count === 0) {
+  backgroundColor = "#f1abb1"; // red
+} else if (count >= 3 && count <= 5) {
+  backgroundColor = "#deca87"; // yellow
+} else if (count > 5) {
+  backgroundColor = "#bbeec7"; // green
+}
 
   return (
-    <div 
-      style={{ 
-        textAlign: "center", 
-        marginTop: "50px",
-        backgroundColor: backgroundColor,
-        minHeight: "100vh",
-        paddingTop: "50px"
-      }}
+    <div
+    style={{
+    textAlign: "center",
+    marginTop: "50px",
+    backgroundColor: backgroundColor,
+    minHeight: "100vh",
+    paddingTop: "50px"
+    }}
     >
       <h1>My First React App 🚀</h1>
       <h2>Count: {count}</h2>
