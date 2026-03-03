07c495e0829b602e5ce2f830159360f5ef2db2f4
tushar <tusharlingnurkar@gmail.com>
2026-03-03 22:06:30 +0530

    Added dymanic marginTop logic

--- react-master/src/App.jsx
+++ react-master/src/App.jsx
@@ -5,22 +5,25 @@ function App() {
 
   let backgroundColor = "#e2abe4";
   let textColor = "#000000";
+  let marginTop = "50px";
 
 if (count === 0) {
   backgroundColor = "#f1abb1"; // red
   textColor = "#14b441";
 } else if (count >= 3 && count <= 5) {
   backgroundColor = "#deca87"; // yellow
+  marginTop = "100px"
 } else if (count > 5) {
   backgroundColor = "#bbeec7"; // green
   textColor = "#d21717";
+  marginTop = "150px"
 }
 
   return (
     <div
     style={{
     textAlign: "center",
-    marginTop: "50px",
+    marginTop: marginTop,
     backgroundColor: backgroundColor,
     color: textColor,
     minHeight: "100vh",
