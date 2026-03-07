b6793096b71509a6ad5aa8cff11234d1dfc69c65
tushar <tusharlingnurkar@gmail.com>
2026-03-07 11:21:33 +0530

    refactored counter into separate component

--- react-master/src/App.jsx
+++ react-master/src/App.jsx
@@ -1,53 +1,13 @@
-import { useState } from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  let backgroundColor = "#e2abe4";
-  let textColor = "#000000";
-  let marginTop = "50px";
-
-if (count === 0) {
-  backgroundColor = "#f1abb1"; // red
-  textColor = "#14b441";
-} else if (count >= 3 && count <= 5) {
-  backgroundColor = "#deca87"; // yellow
-  marginTop = "100px"
-} else if (count > 5) {
-  backgroundColor = "#bbeec7"; // green
-  textColor = "#d21717";
-  marginTop = "150px"
-}
-
   return (
-    <div
-    style={{
-    textAlign: "center",
-    marginTop: marginTop,
-    backgroundColor: backgroundColor,
-    color: textColor,
-    minHeight: "100vh",
-    paddingTop: "50px",
-    transition: "all 0.5s ease"
-    }}
-    >
-      <h1>My First React App 🚀</h1>
-      <h2>Count: {count}</h2>
+    <div>
+      <h1 style={{ textAlign: "center" }}>
+        My React Training App 🚀
+      </h1>
 
-      <button onClick={() => setCount(count + 1)}>
-        Increase
-      </button>
-
-      <button 
-        onClick={() => setCount(prev => prev > 0 ? prev - 1 : 0)}
-        disabled={count === 0}
-      >
-        Decrease
-      </button>
-
-      <button onClick={() => setCount(0)}>
-        Reset
-      </button>
+      <Counter />
     </div>
   );
 }
